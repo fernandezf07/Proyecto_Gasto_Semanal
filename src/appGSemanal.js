3a47ecdc7de6b369bb508cb8f9361b510c2e7249
facundo fernandez <fernandez.facundo94@live.com>
2020-11-03 03:29:04 -0300

    cambios y a mimir

--- src/appGSemanal.js
+++ src/appGSemanal.js
@@ -2,16 +2,20 @@
 
 const addError = document.querySelector('.addError');
 let presupuesto = document.getElementById('presupuesto');
-const gastoNombre = document.querySelector('#gasto-nombre').value;
-const cantidadGasto = document.querySelector('#gasto-cantidad').value;
+const gastoNombre = document.querySelector('#gasto-nombre');
+const cantidadGasto = document.querySelector('#gasto-cantidad');
 const button = document.querySelector('#presubtn');
+const buttonGasto = document.getElementById('gastobtn');
 
-
+// capturando eventos y aplicando funciones
 button.addEventListener('click', addBudge);
+buttonGasto.addEventListener('click', addSpend)
 
 // valida e imprime el presupuesto en pantalla
 function addBudge (e) {
+
 	e.preventDefault();
+
 	const budge = document.getElementById('printBudge');
 	
 	if (presupuesto.value < 1 ) {
@@ -23,7 +27,25 @@ function addBudge (e) {
 		
 }
 
-// muestra el error de validacion y lo muestra por 3 segundos.
+
+function addSpend (e) {
+
+	e.preventDefault();
+	const valorNombre = gastoNombre.value;
+	let gasto = cantidadGasto.value;
+	const pintarRest = document.querySelector('.rest');
+	
+	let restante;
+
+	if (cantidadGasto.value < 1 || gastoNombre.length === 0) {
+	    showError('Complete todos los campos');
+	} else {
+		  pintarRest.style.display = 'flex';
+	}
+	
+}
+
+// muestra el error de validacion  por 2.5 segundos.
 function showError (msg) {
 	
 	const messageError = document.createElement('p');
@@ -34,15 +56,12 @@ function showError (msg) {
 	messageError.style.marginTop = '8px';
 
 	const errors = document.querySelectorAll('.error')
+
 	if (errors.length === 0) {
        addError.appendChild(messageError);
 	}
 	setTimeout(() => {
 		
 		messageError.remove();
-	}, 3000);
-}
-
-function showSpend () {
-
+	}, 2500);
 }
