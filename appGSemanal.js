0bc2b7a5a68a920c1309351acf08b2e074eb826a
facundo fernandez <fernandez.facundo94@live.com>
2020-11-02 23:53:32 -0300

    terminando validacion del campo presupuesto

--- appGSemanal.js
+++
@@ -1,7 +0,0 @@
-//variables globales
-
-const presupuesto = document.querySelector('#presupuesto').value;
-const gastoNombre = document.querySelector('#gasto-nombre').value;
-const cantidadGasto = document.querySelector('#gasto-cantidad').value;
-
-
